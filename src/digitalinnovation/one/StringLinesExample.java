a3d17ad13247d6d12db5a0447be7f4aed3807c48
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-09-02 10:06:14 -0300

    lines()

---
+++ src/digitalinnovation/one/StringLinesExample.java
@@ -0,0 +1,15 @@
+package digitalinnovation.one;
+
+import java.util.stream.Collectors;
+
+public class StringLinesExample {
+
+    public static void main(String[] args) {
+        String html = "<html>\n<head>\n</head>\n<body>\n</body>\n</html>";
+        System.out.println(html
+                .lines()
+                .map(s -> "[TAG]: " + s + "\n")
+                .collect(Collectors.joining())
+        );
+    }
+}
