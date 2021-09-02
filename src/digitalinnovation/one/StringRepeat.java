220b27504d899946a0466ca03ae9de8389300dd7
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-09-02 10:33:58 -0300

    repeat()

---
+++ src/digitalinnovation/one/StringRepeat.java
@@ -0,0 +1,17 @@
+package digitalinnovation.one;
+
+public class StringRepeat {
+
+    public static void main(String[] args) {
+        String name = "Ricky";
+        /*
+        String aux = "";
+        for (int i = 0; i < 10; i++) {
+            aux += name;
+        }
+        System.out.println(aux);
+        */
+
+        System.out.println(name.repeat(10));
+    }
+}
