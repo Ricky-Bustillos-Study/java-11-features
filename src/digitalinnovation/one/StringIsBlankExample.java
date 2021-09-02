628311b7bf020c6506627e71c5a30a61f4c722ef
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-09-02 09:55:53 -0300

    isBlank()

---
+++ src/digitalinnovation/one/StringIsBlankExample.java
@@ -0,0 +1,9 @@
+package digitalinnovation.one;
+
+public class StringIsBlankExample {
+
+    public static void main(String[] args) {
+        String space = " ";
+        System.out.println(space.isBlank());
+    }
+}
